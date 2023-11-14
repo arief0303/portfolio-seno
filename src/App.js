ad04c49088d9106006e3a9922896300fc81375d4
Arief R Syauqie <arief0303@gmail.com>
2023-11-14 22:25:29 +0700

    maket

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { OrbitControls } from '@react-three/drei';
+import { Box } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { useLoader, useThree } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { Environment } from '@react-three/drei'
 
 function App() {
+  const gltf = useLoader(GLTFLoader, '/maket.glb')
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App" style={{width:"100vw", height:"100vh"}}>
+      <Canvas>
+        {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +26,19 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+        <OrbitControls
+          enableDamping
+          enablePan
+          enableRotate
+          enableZoom
+        />
+        {/* <Box>
+          <meshBasicMaterial wireframe />
+        </Box> */}
+        <Environment preset="city" />
+        <primitive object={gltf.scene} />
+      </Canvas>
     </div>
   );
 }
